9652e31861f6e32d140b18e8ebbcbf64f101d4a5
Björn Ekryd <bjorn.ekryd@gmail.com>
2013-10-16 08:19:56 +0200

    Removed complex method

--- src/main/java/refutils/FieldHelper.java
+++ src/main/java/refutils/FieldHelper.java
@@ -1,9 +1,7 @@
 package refutils;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Contains utility methods to get and set field value from an instance of a class. The field can be located by class
@@ -14,7 +12,20 @@ class FieldHelper {
     private final Collection<Field> allFields;
 
     private final Field field;
+    private static final Map<Class,Class> PRIMITIVE_CLASSES_MAP = new LinkedHashMap<Class, Class>();
 
+    static {
+        // Populate map with all java primitives and their corresponding autoboxing class.
+        PRIMITIVE_CLASSES_MAP.put(int.class, Integer.class);
+        PRIMITIVE_CLASSES_MAP.put(long.class, Long.class);
+        PRIMITIVE_CLASSES_MAP.put(double.class, Double.class);
+        PRIMITIVE_CLASSES_MAP.put(float.class, Float.class);
+        PRIMITIVE_CLASSES_MAP.put(byte.class, Byte.class);
+        PRIMITIVE_CLASSES_MAP.put(char.class, Character.class);
+        PRIMITIVE_CLASSES_MAP.put(boolean.class, Boolean.class);
+        PRIMITIVE_CLASSES_MAP.put(short.class, Short.class);
+    }
+    
     /**
      * Locates matching field in instance by looking at field name.
      *
@@ -92,37 +103,10 @@ class FieldHelper {
         return fieldNames.toString();
     }
 
-    @SuppressWarnings("RedundantIfStatement")
     private boolean isMatchedPrimitive(Class<?> fieldType) {
-        if (matchPrimitive(fieldType, int.class, Integer.class)) {
-            return true;
-        }
-        if (matchPrimitive(fieldType, long.class, Long.class)) {
-            return true;
-        }
-        if (matchPrimitive(fieldType, double.class, Double.class)) {
-            return true;
-        }
-        if (matchPrimitive(fieldType, float.class, Float.class)) {
-            return true;
-        }
-        if (matchPrimitive(fieldType, byte.class, Byte.class)) {
-            return true;
-        }
-        if (matchPrimitive(fieldType, char.class, Character.class)) {
-            return true;
-        }
-        if (matchPrimitive(fieldType, boolean.class, Boolean.class)) {
-            return true;
-        }
-        if (matchPrimitive(fieldType, short.class, Short.class)) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean matchPrimitive(Class<?> fieldType, Class<?> primitiveClass, Class<?> boxedPrimitiveClass) {
-        return boxedPrimitiveClass.equals(valueClass) && primitiveClass.equals(fieldType);
+        Class boxedPrimitiveClass = PRIMITIVE_CLASSES_MAP.get(fieldType);
+        
+        return boxedPrimitiveClass != null && boxedPrimitiveClass.equals(valueClass);
     }
 
     /**
